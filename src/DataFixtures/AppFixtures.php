7437635f89c0cbe41c3ee186ac588d2e7b52613d
bramsT96 <119591307+bramsT96@users.noreply.github.com>
2023-05-28 23:03:55 +0200

    nouvellefixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use app\Entity\Destination;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i=0; $i < 5; $i++) { 
+            $destination = new Destination();
+            $destination->setNom('Destination '.$i)
+                        ->setVille('Ville '.$i)
+                        ->setPays('Pays '.$i)
+                        ->setDescription('Description de la destination '.$i)
+                        ->setPrix(100)
+                        ->setDevise('EUR')
+                        ->setDateCreation(new \DateTime('2023-05-01'))
+                        ->setDateMAJ(new \DateTime('2023-05-20'));
+            $manager->persist($destination);
+        }
 
         $manager->flush();
     }
